791fec6ea2f90862ca07595398720546af4d41ea
helloyesterday <yesterday.young@gmail.com>
2022-07-16 16:06:19 +0800

    add Cybertron

---
+++ papers/cybertroncode/README.md
@@ -0,0 +1,67 @@
+# Cybertron
+
+#### 介绍
+
+Cybertron：基于MindSpore的深度分子模型通用架构
+
+本程序由深圳湾实验室、北京大学、昌平实验室与华为MindSpore团队共同开发
+
+开发人员：杨奕，张骏，陈迪青，张辉耀，周亚强，雷耀坤，杨立江，高毅勤等
+
+联系方式: yangyi@szbl.ac.cn
+
+#### 软件架构
+
+本程序为深度分子模型通用程序架构，可以支持基于图神经网络的深度分子模型。
+
+目前本程序内置三种GNN分子模型：SchNet[1]、PhysNet[2]以及MolCT[3]。
+
+参考文献：
+
+[1] https://arxiv.org/abs/1706.08566
+
+[2] https://arxiv.org/abs/1902.08408
+
+[3] https://arxiv.org/abs/2012.11816 
+
+#### 安装教程
+
+本程序基于华为全场景人工智能框架MindSpore开发，使用前请先安装MindSpore：https://mindspore.cn/
+
+#### 使用说明
+
+根目录下的cybertron_tutorial_CN.pdf文件为程序说明，examples目录内为使用案例：
+
+Tutorial_00.py：数据预处理
+
+Tutorial_01.py：基础教程（一）
+
+Tutorial_02.py：基础教程（二）
+
+Tutorial_03.py：归一化数据集与验证数据集的使用
+
+Tutorial_04.py：模型参数与超参数的读取（一）
+
+Tutorial_05.py：多任务训练（一）
+
+Tutorial_06.py：多任务训练（二）
+
+Tutorial_07.py：带有力的数据集的拟合
+
+Tutorial_08.py：模型参数与超参数的读取（二） 
+
+注：Tutorial_00.py所需数据集可从以下网址下载：
+
+    dataset_qm9.npz: http://gofile.me/6Utp7/tJ5hoDIAo
+
+    ethanol_dft.npz: http://gofile.me/6Utp7/hbQBofAFM
+
+使用Tutorial_00.py处理之后生成的数据集已在examples文件夹中，并可用于Tutorial_01.py ~ Tutorial_08.py。
+
+#### 参与贡献
+
+1.  Fork 本仓库
+2.  新建 Feat_xxx 分支
+3.  提交代码
+4.  新建 Pull Request
+
---
+++ papers/cybertroncode/examples/tutorial_01.py
@@ -0,0 +1,100 @@
+# Copyright 2020-2022 The AIMM team at Shenzhen Bay Laboratory & Peking University
+#
+# People: Yi Isaac Yang, Jun Zhang, Diqing Chen, Yaqiang Zhou, Huiyang Zhang,
+#         Yupeng Huang, Yijie Xia, Yao-Kun Lei, Lijiang Yang, Yi Qin Gao
+#
+# Contact: yangyi@szbl.ac.cn
+#
+# Tutorials for Cybertron
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""
+Tutorial 01: Quick introduction of Cybertron
+"""
+
+if __name__ == '__main__':
+
+    import time
+    import numpy as np
+    from mindspore import nn
+    from mindspore import context
+    from mindspore import dataset as ds
+    from mindspore.train.callback import LossMonitor
+    from mindspore.train.callback import ModelCheckpoint, CheckpointConfig
+    from mindspore.train import Model
+
+    import sys
+    sys.path.append('..')
+
+    from cybertron.cybertron import Cybertron
+    from cybertron.train import WithLabelLossCell
+
+    context.set_context(mode=context.GRAPH_MODE, device_target="GPU")
+
+    train_file = sys.path[0] + '/dataset_qm9_origin_trainset_1024.npz'
+
+    train_data = np.load(train_file)
+
+    idx = [0]  # diple
+
+    num_atom = int(train_data['num_atoms'])
+    scale = train_data['scale'][idx]
+    shift = train_data['shift'][idx]
+
+    net = Cybertron(model='schnet', readout='graph', dim_output=1,
+                    num_atoms=num_atom, length_unit='nm', energy_unit='kj/mol')
+    net.set_scaleshift(scale, shift)
+
+    net.print_info()
+
+    tot_params = 0
+    for i, param in enumerate(net.get_parameters()):
+        tot_params += param.size
+        print(i, param.name, param.shape)
+    print('Total parameters: ', tot_params)
+
+    n_epoch = 8
+    repeat_time = 1
+    batch_size = 32
+
+    ds_train = ds.NumpySlicesDataset(
+        {'R': train_data['R'], 'Z': train_data['Z'], 'E': train_data['E'][:, idx]}, shuffle=True)
+    ds_train = ds_train.batch(batch_size, drop_remainder=True)
+    ds_train = ds_train.repeat(repeat_time)
+    loss_network = WithLabelLossCell('RZE', net, nn.MAELoss())
+
+    lr = 1e-3
+    optim = nn.Adam(params=net.trainable_params(), learning_rate=lr)
+
+    model = Model(loss_network, optimizer=optim)
+
+    monitor_cb = LossMonitor(16)
+
+    outdir = 'Tutorial_01'
+    params_name = outdir + '_' + net.model_name
+    config_ck = CheckpointConfig(
+        save_checkpoint_steps=32, keep_checkpoint_max=64, append_info=[net.hyper_param])
+    ckpoint_cb = ModelCheckpoint(
+        prefix=params_name, directory=outdir, config=config_ck)
+
+    print("Start training ...")
+    beg_time = time.time()
+    model.train(n_epoch, ds_train, callbacks=[
+                monitor_cb, ckpoint_cb], dataset_sink_mode=False)
+    end_time = time.time()
+    used_time = end_time - beg_time
+    m, s = divmod(used_time, 60)
+    h, m = divmod(m, 60)
+    print("Training Fininshed!")
+    print("Training Time: %02d:%02d:%02d" % (h, m, s))
