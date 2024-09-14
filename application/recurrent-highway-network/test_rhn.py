b480203b374e7db3ec7ff549153f36d0141f99ac
ttfa <heyuheng@mail.ustc.edu.cn>
2024-09-14 09:02:39 +0800

    add recurrent-network

--- application/recurrent-highway-network/test_rhn.py
+++ application/recurrent-highway-network/test_rhn.py
@@ -1,7 +1,5 @@
 from recurrent_highway_network import RHN
-# import mindspore
 from mindspore import ops
-# import numpy
 
 batch_size = 2
 seq_len = 1000
@@ -13,7 +11,7 @@ nb_rhn_layers = 5
 input = ops.randn([batch_size, seq_len, input_dim])
 rhn = RHN(input_dim, hidden_dim, nb_rhn_layers, drop_prob=0.1)
 
-# rhn.train()
+
 s_t_0_l_0 = ops.zeros([batch_size, hidden_dim])
 output = rhn(input, s_t_0_l_0)
 print(output)
