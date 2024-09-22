06b042528649c3b15dee755e20d45373f2823943
Eliwii_Keeya <Eliwii_Keeya@163.com>
2024-09-22 16:17:15 +0800

    chore(Cell): 重构部分代码
    本次提交重构部分函数, 不影响函数功能。

--- application/hyperspherical-consistency-regularization/hcr.py
+++ application/hyperspherical-consistency-regularization/hcr.py
@@ -8,7 +8,6 @@ class HCR:
         self.weight = weight
         self.optimizer = optimizer_func(
             classifier_network.trainable_params(), lr)
-        self.loss = None
 
     def pairwise_dist(self, x):
         x_square = x.pow(2).sum(axis=1)
@@ -26,39 +25,39 @@ class HCR:
             h)), self.pairwise_prob(self.pairwise_dist(g))
         return -1 * (q1 * ops.log(q2 + self.eps)).mean() + -1 * ((1 - q1) * ops.log((1 - q2) + self.eps)).mean()
 
-    def hcr_loss_weight(self, h, g):
-        return self.hcr_loss(h, g) * self.weight
+    def loss_feat(self, logits, projections):
+        return self.hcr_loss(ops.renorm(logits, p=2, axis=0, maxnorm=1.), ops.stop_gradient(ops.renorm(projections, p=2, axis=0, maxnorm=1.))) * self.weight
 
     def update(self, logits, projections):
         grad_fn = mindspore.value_and_grad(
-            self.hcr_loss_weight, None, self.optimizer.trainable_params())
-        self.loss, grads = grad_fn(logits, projections)
-        self.optimizer(grads)
+            self.loss_feat, None, self.optimizer.trainable_params())
+        loss, grads = grad_fn(logits, projections)
+        loss = self.optimizer(grads)
+        return loss
 
 
 class LeNet5(nn.Cell):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 6, 5, pad_mode='pad')
-        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool1 = nn.MaxPool2d(2, 2, pad_mode='pad')
         self.conv2 = nn.Conv2d(6, 16, 5, pad_mode='pad')
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.SequentialCell(
-            nn.Flatten(1, -1),
-            nn.Dense(400, 120)
-        )
+        self.pool2 = nn.MaxPool2d(2, 2, pad_mode='pad')
+        self.fc1 = nn.Dense(400, 120)
         self.fc2 = nn.Dense(120, 84)
-        self.output = nn.Dense(84, 10)
+        self.fc3 = nn.Dense(84, 10)
         self.relu = nn.ReLU()
+        self.flatten = nn.Flatten(1, -1)
 
     def construct(self, x):
         x = self.conv1(x)
         x = self.pool1(self.relu(x))
         x = self.conv2(x)
         x = self.pool2(self.relu(x))
+        x = self.flatten(x)
         x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.output(x)
+        x = self.fc2(self.relu(x))
+        x = self.fc3(self.relu(x))
         return x
 
 
@@ -67,12 +66,11 @@ if __name__ == '__main__':
     mindspore.run_check()
 
     # define & configure the network
-    network = LeNet5()
-    optimizer = nn.Adam
-
     learning_rate = 0.01
     num_epochs = 20
-    hcr_reg = HCR(network, learning_rate, optimizer)
+    network = LeNet5()
+    optimizer_func = nn.Adam
+    hcr_reg = HCR(network, learning_rate, optimizer_func)
 
     # generate dataset
     BATCH_SIZE = 8
@@ -80,11 +78,13 @@ if __name__ == '__main__':
     HEIGHT = 32
     WEIGHT = 32
     NUM_LABELS = 10
-
     data = ops.rand([BATCH_SIZE, NUM_CHENNELS, HEIGHT, WEIGHT])
-    labels = ops.randint(0, 9, (BATCH_SIZE, NUM_LABELS)
-                         ).type(mindspore.float32)
+    labels_idx = ops.randint(0, 9, (BATCH_SIZE, 1)).squeeze(-1)
+    labels = ops.zeros([BATCH_SIZE, NUM_LABELS], dtype=mindspore.float32)
+    labels[ops.arange(BATCH_SIZE, dtype=mindspore.int16), labels_idx] = 1.
+    del labels_idx
 
+    # calculate
     logits = network(data)
     loss = hcr_reg.hcr_loss(logits, labels)
     print(f"loss: {loss}")
