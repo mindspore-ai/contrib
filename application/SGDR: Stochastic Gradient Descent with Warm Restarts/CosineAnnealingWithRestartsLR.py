db6748a0b4b790a7dc396223e41e7a7096e501c2
kaydam <1121743758@qq.com>
2024-09-26 07:28:58 +0000

    【代码迁移】任务39 : SGDR: Stochastic Gradient Descent with Warm Restarts

--- application/SGDR: Stochastic Gradient Descent with Warm Restarts/CosineAnnealingWithRestartsLR.py
+++ application/SGDR: Stochastic Gradient Descent with Warm Restarts/CosineAnnealingWithRestartsLR.py
@@ -1,39 +1,42 @@
 import math
 import matplotlib.pyplot as plt
+from mindspore.nn.learning_rate_schedule import LearningRateSchedule
+import mindspore
 
 
-class CosineAnnealingWithRestartsLR:
-    def __init__(self, T_max, eta_min=0, T_mult=1):
+class CosineAnnealingWithRestartsLR(LearningRateSchedule):
+    def __init__(self, T_max, eta_min=0, T_mult=1, base_lrs=[1e3]):
+        super(CosineAnnealingWithRestartsLR, self).__init__()
         self.T_max = T_max
         self.T_mult = T_mult
         self.next_restart = T_max
         self.eta_min = eta_min
         self.restarts = 0
         self.last_restart = 0
+        self.base_lrs = base_lrs
 
-    def get_lr(self, base_lrs, cur_epoch):
-        self.t_cur = cur_epoch - self.last_restart
+    def construct(self, current_step):
+        self.t_cur = current_step - self.last_restart
         if self.t_cur >= self.next_restart:
             self.next_restart *= self.T_mult
-            self.last_restart = cur_epoch
-        return [
+            self.last_restart = current_step
+        return mindspore.Tensor([
             (
                     self.eta_min
                     + (base_lr - self.eta_min)
                     * (1 + math.cos(math.pi * self.t_cur / self.next_restart))
                     / 2
             )
-            for base_lr in base_lrs
-        ]
+            for base_lr in self.base_lrs
+        ])
 
 
-# 示例用法和绘图
 if __name__ == "__main__":
     scheduler = CosineAnnealingWithRestartsLR(T_max=30, T_mult=1.5)
     lrs = []
     for step in range(240):
-        lr = scheduler.get_lr([1e-3], step)
+        lr = scheduler(step)
         lrs.append(lr)
-
     plt.plot(lrs)
-    plt.savefig('cosine_annealing_with_restarts_lr.png')
+    plt.savefig(
+        'cosine_annealing_with_restarts_lr.png')
